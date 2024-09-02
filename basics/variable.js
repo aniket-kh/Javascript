06b6232477d91fb5a3e7b9f25133e7244fef6e0f
aniket-kh <107784370+aniket-kh@users.noreply.github.com>
2024-09-02 07:24:03 +0000

    variable done

---
+++ basics/variable.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "aniket@google.com"
+var accountPass = "12345"
+ accountCity = "pune"
+ let accountState;
+// accountId = 2
+// console.log(accountId);
+// dont use var keyword 
+
+ accountEmail= "Ak@gmail.com"
+ accountPass = "4144"
+ accountCity = "Walhe"
+
+ console.table([accountId,accountEmail,accountPass,accountState,accountCity]);
+ 
