b594a1b908cfb86c2f91005c785964099c9b7e57
aniket-kh <107784370+aniket-kh@users.noreply.github.com>
2024-09-03 05:27:30 +0000

    datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,23 @@
+
+"use strict"
+
+let name = "Aniket"
+let age = 27
+let isLoggedIn = true
+console.log(typeof isLoggedIn);
+// premitive data types 
+//  String
+// number
+// boolean
+// int 
+// Bigint
+// null
+// undefined
+console.log(typeof undefined); 
+console.log(typeof null)
+
+// non premitive data types 
+
+// object
+// function
+// array
